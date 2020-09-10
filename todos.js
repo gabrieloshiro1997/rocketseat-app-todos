0c48c407876a80306b093f2c0e1fb383c5c7995d
Gabriel Oshiro <gabriel.oshiro@cbyk.com.br>
2020-09-10 09:15:38 -0300

    Criando todos

--- todos.js
+++ todos.js
@@ -9,6 +9,8 @@ var todos = [
 ];
 
 function renderTodos() {
+	listElement.innerHTML = '';
+	
 	for (todo of todos) {
 		var todoElement = document.createElement('li');
 		var todoText = document.createTextNode(todo);
@@ -18,4 +20,14 @@ function renderTodos() {
 	}
 }
 
-renderTodos()
+renderTodos();
+
+function addTodo() {
+	var todoText = inputElement.value;
+
+	todos.push(todoText);
+
+	inputElement.value = '';
+
+	renderTodos();
+}
